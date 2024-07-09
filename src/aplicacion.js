48b1dc40919717aa6f67016094369e08883da92c
BidoMa <77677614+BidoMa@users.noreply.github.com>
2024-07-09 21:25:53 +0200

    Update aplicacion.js

--- src/aplicacion.js
+++ src/aplicacion.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
+import DashboardFranmaker from './DashboardFranmaker';
 
+const Aplicacion = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <DashboardFranmaker />
+      ) : (
+        <div>
+          <h1>Bienvenido a FranMaker</h1>
+          <button onClick={() => setIsAuthenticated(true)}>Iniciar Sesión</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Aplicacion;
