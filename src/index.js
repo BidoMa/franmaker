2ee56d8c23c2543811f53f95c9d912503469b731
BidoMa <77677614+BidoMa@users.noreply.github.com>
2024-07-09 21:31:37 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Asegúrate de que este archivo existe
+import Aplicacion from './aplicacion';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Aplicacion />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
